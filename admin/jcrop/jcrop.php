b43c0d538f20e41b4c24767fddcc2fe126e9459b
Joan Gimenez Donat <joan16v@gmail.com>
2017-11-17 08:10:57 +0100

    Update jcrop.php

--- admin/jcrop/jcrop.php
+++ admin/jcrop/jcrop.php
@@ -1,52 +1,39 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-	//$targ_w = $targ_h = 150;
-    $targ_w=    $_POST['w'];
-    $targ_h=    $_POST['h'];
-	$jpeg_quality = 90;
-
-	$src = "../".$_POST['fichero'];
-	$img_r = imagecreatefromjpeg($src);
-	$dst_r = ImageCreateTrueColor( $targ_w, $targ_h );
-
-	imagecopyresampled($dst_r,$img_r,0,0,$_POST['x'],$_POST['y'],
-	$targ_w,$targ_h,$_POST['w'],$_POST['h']);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $targ_w = $_POST['w'];
+    $targ_h = $_POST['h'];
+    $jpeg_quality = 90;
+    $src = "../" . $_POST['fichero'];
+    $img_r = imagecreatefromjpeg($src);
+    $dst_r = ImageCreateTrueColor($targ_w, $targ_h);
+    imagecopyresampled($dst_r, $img_r, 0, 0, $_POST['x'], $_POST['y'], $targ_w, $targ_h, $_POST['w'], $_POST['h']);
 
     ?>
     <html>
-    <head>
-    <title>Subir fotos al servidor</title>
-    <link rel="stylesheet" href="admin_style.css" type="text/css" />
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    
-    <script type='text/javascript' src='../js/jquery.min.js'></script>
-    
-    </head>
-    <body>
-    
-    <div style="margin: 20px;">
-        <h1>Subir fotos al servidor con jQuery CROP (beta)</h1>    
-        
-        <div>Foto subida con éxito.</div>
-        
-        <div>
-            <img src="<? echo $src; ?>" />
-        </div>
-        
-        <div>
-            <input type="button" value="OK" onclick="window.location='../index.php'" />
-        </div>
-        
-    </div>    
-    </body>
-    </html>            
-    <?
-	header('Content-type: image/jpeg');
-	imagejpeg($dst_r,null,$jpeg_quality);
+        <head>
+            <title>Subir fotos al servidor</title>
+            <link rel="stylesheet" href="admin_style.css" type="text/css" />
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+            <script type='text/javascript' src='../js/jquery.min.js'></script>
+        </head>
+        <body>
+            <div style="margin: 20px;">
+                <h1>Subir fotos al servidor con jQuery CROP (beta)</h1>
+                <div>Foto subida con éxito.</div>
+                <div>
+                    <img src="<?php echo $src; ?>" />
+                </div>
+                <div>
+                    <input type="button" value="OK" onclick="window.location='../index.php'" />
+                </div>
+            </div>
+        </body>
+    </html>
+    <?php
 
-	exit;
+    header('Content-type: image/jpeg');
+    imagejpeg($dst_r, null, $jpeg_quality);
+
+    exit;
 }
-
-?>

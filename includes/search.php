8100b65c63cf22588e1ec1b4f78be11867141746
Joan Gimenez Donat <joan16v@gmail.com>
2017-12-19 12:11:16 +0100

    Update search.php

--- includes/search.php
+++ includes/search.php
@@ -1,12 +1,11 @@
-<!-- SEARCH BOX -->
 <div style="margin-top: 20px;">
     <form action="index.php" method="get">
-    <div style="position: absolute; top: 0px; left:-4000px;;"><input type="submit" /></div>
-    <table>
-        <tr>
-            <td><img src="template/magnifier.png" /></td>
-            <td><input type="text" name="search" value="<? echo @$_GET['search']; ?>" /></td>            
-        </tr>
-    </table>
+        <div style="position: absolute; top: 0px; left:-4000px;"><input type="submit" /></div>
+        <table>
+            <tr>
+                <td><img src="template/magnifier.png" /></td>
+                <td><input type="text" name="search" value="<?php echo @$_GET['search']; ?>" /></td>
+            </tr>
+        </table>
     </form>
 </div>

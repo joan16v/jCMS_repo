6d92e1c00a2067447b43d81243e20394208ad4a2
Joan Gimenez Donat <joan16v@gmail.com>
2017-12-20 08:45:49 +0100

    Update title.php

--- includes/title.php
+++ includes/title.php
@@ -1,37 +1,41 @@
-<? 
+<?php
 
-// SEO title
+if (isset($_GET['id'])) {
+    $id_seccion = GetSQLValueString($_GET['id'], "int");
+    $rowSeccion = mysql_fetch_object(mysql_query("select * from jcms_secciones where id='" . $id_seccion . "'"));
+    $nombre = $rowSeccion->nombre;
 
-//seccion
-if(isset($_GET['id'])) {
-    $id_seccion=GetSQLValueString($_GET['id'],"int");
-    $rowSeccion=mysql_fetch_object(mysql_query("select * from jcms_secciones where id='".$id_seccion."'"));
-    $nombre=$rowSeccion->nombre;
-    if( $_SESSION["lang_session"]=="EN" ) $nombre=$rowSeccion->nombre_en; 
-    echo $nombre." - ".PAGE_TITLE;
+    if ($_SESSION["lang_session"] == "EN") {
+        $nombre = $rowSeccion->nombre_en;
+    }
+
+    echo $nombre . " - " . PAGE_TITLE;
 }
 
-//producto
-if(isset($_GET['id_prod'])) {
-    $id_producto=GetSQLValueString($_GET['id_prod'],"int");
-    $rowProducto=mysql_fetch_object(mysql_query("select * from jcms_productos where id='".$id_producto."'"));    
-    $nombre=$rowProducto->nombre;
-    if( $_SESSION["lang_session"]=="EN" ) $nombre=$rowProducto->nombre_en;
-    echo $nombre." - ".PAGE_TITLE;
+if (isset($_GET['id_prod'])) {
+    $id_producto = GetSQLValueString($_GET['id_prod'], "int");
+    $rowProducto = mysql_fetch_object(mysql_query("select * from jcms_productos where id='" . $id_producto . "'"));
+    $nombre = $rowProducto->nombre;
+
+    if ($_SESSION["lang_session"] == "EN") {
+        $nombre = $rowProducto->nombre_en;
+    }
+
+    echo $nombre . " - " . PAGE_TITLE;
 }
 
-//noticia
-if(isset($_GET['id_not'])) {
-    $id_noticia=GetSQLValueString($_GET['id_not'],"int");
-    $rowNoticia=mysql_fetch_object(mysql_query("select * from jcms_noticias where id='".$id_noticia."'"));    
-    $nombre=$rowNoticia->nombre;
-    if( $_SESSION["lang_session"]=="EN" ) $nombre=$rowNoticia->nombre_en;
-    echo $nombre." - ".PAGE_TITLE;
+if (isset($_GET['id_not'])) {
+    $id_noticia = GetSQLValueString($_GET['id_not'], "int");
+    $rowNoticia = mysql_fetch_object(mysql_query("select * from jcms_noticias where id='" . $id_noticia . "'"));
+    $nombre = $rowNoticia->nombre;
+
+    if ($_SESSION["lang_session"] == "EN") {
+        $nombre = $rowNoticia->nombre_en;
+    }
+
+    echo $nombre . " - " . PAGE_TITLE;
 }
 
-//default
-if( !isset($_GET['id']) && !isset($_GET['id_prod']) && !isset($_GET['id_not']) ) {
+if (!isset($_GET['id']) && !isset($_GET['id_prod']) && !isset($_GET['id_not'])) {
     echo PAGE_TITLE;
 }
-
-?>
